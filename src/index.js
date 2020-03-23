a89f9b97d5e835a58d7aa9de4474912fa233ffd5
Eric Fan <ericfan2770@gmail.com>
2020-03-23 15:55:53 -0700

    Part 12: Composing Components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 // import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css"; // import bootstrap.css
-import Counter from "./components/counter"; // import Counter class
+import Counters from "./components/counters"; // import Counter class
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
