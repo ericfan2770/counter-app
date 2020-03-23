89fb5a2d47eb836d810e8c3f2942b467bd0840be
Eric Fan <ericfan2770@gmail.com>
2020-03-23 16:08:21 -0700

    Passing Data to Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   // special property, state contains any data that the component needs
   state = {
-    count: 0
+    value: this.props.value
   };
 
   //   constructor() {
@@ -16,7 +16,7 @@ class Counter extends Component {
     // console.log("Increment Clicked", this);
     // this.state.count++;
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   //   doHandleIncrement = () => {
@@ -24,6 +24,8 @@ class Counter extends Component {
   //   };
 
   render() {
+    // console.log("props", this.props);
+
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -40,12 +42,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -15,7 +15,7 @@ class Counters extends Component {
     return (
       <React.Fragment>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </React.Fragment>
     );
