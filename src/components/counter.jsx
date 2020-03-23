5cf9074d638d214832edc1a562bc43dc9ea3f72d
Eric Fan <ericfan2770@gmail.com>
2020-03-23 14:38:53 -0700

    Part 4: Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,19 @@ import React, { Component } from "react";
 class Counter extends Component {
   // special property, state contains any data that the component needs
   state = {
-    count: 1
+    count: 0
   };
+
+  //   styles = {
+  //     fontSize: 10,
+  //     fontWeight: "bold"
+  //   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
