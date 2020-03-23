75dc737a8c4e8358794d460752d72e3a5ce0e258
Eric Fan <ericfan2770@gmail.com>
2020-03-23 14:58:00 -0700

    Part 7: Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,8 +4,21 @@ class Counter extends Component {
   // special property, state contains any data that the component needs
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return "There are no tags!";
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   //   styles = {
   //     fontSize: 10,
@@ -15,27 +28,24 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */
+        this.renderTags()}
       </React.Fragment>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
+  //   getBadgeClasses() {
+  //     let classes = "badge m-2 badge-";
+  //     classes += this.state.count === 0 ? "warning" : "primary";
+  //     return classes;
+  //   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+  //   formatCount() {
+  //     const { count } = this.state;
+  //     return count === 0 ? "Zero" : count;
+  //   }
 }
 
 export default Counter;
