2df327cb4a9f8c98fe3b00435145289bb484e57e
Eric Fan <ericfan2770@gmail.com>
2020-03-23 17:02:44 -0700

    Part 17: Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    })
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     // console.log("Event Handler Called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -20,6 +28,7 @@ class Counters extends Component {
   render() {
     return (
       <React.Fragment>
+        <button onClick={this.handleReset} className="btn btn-primary btn-small m-2">Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id} // used internally by React, can't be read
